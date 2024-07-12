6dbc3d90d7b017d342ecba6e85e8f31671cf5aa3
Guillaume Clabaut <123895070+GClabaut@users.noreply.github.com>
2024-07-12 12:55:03 +0200

    Basic game controller done

--- index.js
+++ index.js
@@ -1,9 +1,17 @@
 function gameboard () {
-  let gameboard = ["", "", "", "", "" ,"" ,"" , "", ""];
+  let gameboard = [
+    ["", "", ""],
+    ["", "", ""],
+    ["", "", ""]
+  ];
 
   const resetGameboard = () => {
     gameboard.length = 0;
-    gameboard.push("", "", "", "", "" ,"" ,"" , "", "");
+    gameboard.push(
+      ["", "", ""],
+      ["", "", ""],
+      ["", "", ""]
+    );
   };
 
   return { gameboard, resetGameboard }
@@ -14,17 +22,50 @@ function player (playerSign) {
   return { sign }
 }
 
-function gameController () {
-  // 
-}
-
 const player1 = player("X");
 const player2 = player("O");
 
+function gameController () {
+
+  let playBoard = gameboard();
+  let player = player1;
+
+  const changeTurn = () => {
+    player = player === player1 ? player2 : player1;
+  }
+
+  const play = (row, column) => {
+    console.log(player)
+    playBoard.gameboard[row][column] = player.sign;
+    changeTurn();
+    console.log(playBoard.gameboard);
+  }
+
+  return { playBoard, play }
+}
 
 // Example play + reset
-let gameboard2 = gameboard();
-gameboard2.gameboard[1] = player1.sign;
-console.log(gameboard2.gameboard);
-gameboard2.resetGameboard();
-console.log(gameboard2.gameboard);
+// let gameboard2 = gameboard();
+// gameboard2.gameboard[1] = player1.sign;
+// console.log(gameboard2.gameboard);
+// gameboard2.resetGameboard();
+// console.log(gameboard2.gameboard);
+
+// Test gameController, O wins
+const play = gameController();
+console.log(play);
+
+play.play(1,1);
+
+play.play(0,0);
+
+play.play(2,0);
+
+play.play(0,2);
+
+play.play(2,2);
+
+play.play(0,1);
+
+// play.playBoard.resetGameboard();
+// console.log(play.playBoard.gameboard);
